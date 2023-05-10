68dab60966d6c5150605eb124be88f97f309f274
deanaura <103201935+deanaura@users.noreply.github.com>
2023-05-10 08:15:19 +0700

    Add files via upload

---
+++ node-js-moduleBiodata.js
@@ -0,0 +1,11 @@
+const biodata = {
+  nama: "Aura",
+  tempatLahir: "Bekasi",
+  tanggalLahir: "08/05/2003",
+  alamat: "Bekasi",
+};
+
+console.log("Nama :" + biodata.nama);
+console.log("Tempat Lahir :" + biodata.tempatLahir);
+console.log("Tanggal Lahir :" + biodata.tanggalLahir);
+console.log("Alamat :" + biodata.alamat);
